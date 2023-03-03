c482e2bee81bd2d6a0b66397929bdfb7e143ee25
un-know-n <fa1thlezss@gmail.com>
2023-03-03 14:50:11 +0200

    Task01: add doc comments

--- Task01/Main.java
+++ Task01/Main.java
@@ -1,7 +1,14 @@
+/**
+ * Print command-line parameters.
+ */
 public class Main {
+    /**
+     * Program entry point.
+     * @param args command-line parameters list
+     */
     public static void main(String[] args) {
-        for (String s : args) {
-            System.out.println(s);
+        for (String arg: args) {
+            System.out.println(arg);
         }
     }
 }
